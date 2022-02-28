754642f0d593fc521427eb64da3674f2946d89c7
RE <rechezuria@gmail.com>
2022-02-28 09:54:16 -0400

    style: base style

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,23 @@
 <script>
 </script>
 
-<main />
+<main>
+	<section class="container" />
+</main>
 
 <style>
 	:root {
 		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
 			Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+		--base-color: hsl(227, 16%, 22%);
+		--base-rest-country-color: hsl(227, 16%, 42%);
+		--rusia-color: hsl(9, 64%, 50%);
+		--ukraine-color: hsl(40, 98%, 47%);
+		--no-fly-color: hsl(192, 64%, 40%);
+		--no-fly-color-1: #hsl(192, 64%, 62%);
+		--font-color: hsl(212, 22%, 68%);
+
+		background: var(--base-color);
 	}
 </style>
