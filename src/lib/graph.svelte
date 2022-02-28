2eb0e743bb4c90aec58667d3a85083ceca731090
RE <rechezuria@gmail.com>
2022-02-28 10:41:12 -0400

    feature: load world topojson

--- src/lib/graph.svelte
+++ src/lib/graph.svelte
@@ -1,8 +1,24 @@
 <script>
-	let width;
+	import { onMount } from 'svelte';
+	import * as d3 from 'd3';
+	import * as topojson from 'topojson-client';
+
+	let width = 500;
 	$: width = width * 0.95;
-	let height;
+	let height = 500;
 	$: height = height * 0.98;
+	let world;
+	$: console.log('world', world);
+	// Add topojsom
+	onMount(async () => {
+		const resp = await d3.json(
+			'/src/assets/world-administrative-boundaries.json'
+		);
+		world = await topojson.feature(resp, 'world-administrative-boundaries');
+	});
+
+	$: projection = d3.geoOrthographic().fitSize([width, height], world);
+	$: path = d3.geoPath(projection);
 </script>
 
 <svelte:window bind:innerWidth={width} bind:innerHeight={height} />
