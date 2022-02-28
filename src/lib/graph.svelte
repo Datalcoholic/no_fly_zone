307d30a3b13155f95e94b2ea24c1fbef25b30bf0
RE <rechezuria@gmail.com>
2022-02-28 10:06:12 -0400

    feature: add and set graph componet create svg

---
+++ src/lib/graph.svelte
@@ -0,0 +1,19 @@
+<script>
+	let width;
+	$: width = width * 0.95;
+	let height;
+	$: height = height * 0.98;
+</script>
+
+<svelte:window bind:innerWidth={width} bind:innerHeight={height} />
+
+<svg {width} {height}>
+	<slot />
+</svg>
+
+<!-- markup (zero or more items) goes here -->
+<style>
+	svg {
+		border: 1px solid tomato;
+	}
+</style>
