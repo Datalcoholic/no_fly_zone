d1f5552e33963e5d03398c8832d7c6efbb5d950c
RE <rechezuria@gmail.com>
2022-02-28 18:28:07 -0400

    feature: add draggable component

---
+++ src/lib/draggable.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let mouseX;
+	export let mouseY;
+	let isMove = false;
+
+	const start = () => {
+		isMove = true;
+	};
+	const move = (e) => {
+		if (isMove) {
+			mouseX = e.clientX;
+			mouseY = e.clientY;
+		}
+	};
+	const end = () => {
+		isMove = false;
+	};
+</script>
+
+<svelte:window on:mousemove={move} on:mouseup={end} />
+<g class="draggable" on:mousedown={start}>
+	<slot />
+</g>
